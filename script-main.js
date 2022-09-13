19d9aff3477ad739e20b7e3e86696f81ebb8e265
vegardgunnarson <110391678+vegardgunnarson@users.noreply.github.com>
2022-09-13 14:32:47 +0200

    clean code

--- script-main.js
+++ script-main.js
@@ -9,7 +9,11 @@ let computers = [];
 let features = []; 
 let price=20;
 let selectedComputer;
+var ul = "";
 
+/**
+ * Fetching json response from Noroff api
+ */
 fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
     .then(response => response.json())
     .then(data => computers = data)
@@ -18,24 +22,45 @@ fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
 const addComputersToList = (computers) => {
     computers.forEach(c => addComputerToList(c));
 }
-
+/**
+ * Adds computer in selection meny and sets cumputer[0] as default
+ * Stores price and selectedcomputer to be used in other functions
+ * @param {computer} computer 
+ */
 const addComputerToList = (computer) => {
     const computerElement = document.createElement("option");
     computerElement.value = computer.id;
     computerElement.appendChild(document.createTextNode(computer.title));
     computersElement.appendChild(computerElement);
     selectedComputer = computers[0];
-    featuresElement.innerHTML = computers[0].specs;
-    imageElement.src=imageURL+computers[0].image;
-    priceElement.innerText = computers[0].price+" kr";
-    price=computers[0].price;
-    titleElement.innerText = computers[0].title;
-    descriptionElement.innerText = computers[0].description;
+    addFestures(selectedComputer.specs);
+    imageElement.src=imageURL+selectedComputer.image;
+    priceElement.innerText = selectedComputer.price+" kr";
+    price=selectedComputer.price;
+    titleElement.innerText = selectedComputer.title;
+    descriptionElement.innerText = selectedComputer.description;
 }
 
+/**
+ * Takes in features as string and returns as unorded list
+ * @param {features} array 
+ */
+function addFestures (array) {
+    document.getElementById("features").innerHTML="";
+    for (let i of array){
+        let li = document.createElement("li");
+        li.innerHTML = i;
+        document.getElementById("features").appendChild(li);
+    }
+}
+
+/**
+ * Handles changes in selection meny, sets all elements to selected computer values
+ * @param {eventlistener} e 
+ */
 const handleComputerChange = e => {
     selectedComputer = computers[e.target.selectedIndex];
-    featuresElement.innerHTML =selectedComputer.specs;
+    addFestures(selectedComputer.specs);
     priceElement.innerText = selectedComputer.price;
     descriptionElement.innerText = selectedComputer.description;
     titleElement.innerText = selectedComputer.title;
@@ -45,7 +70,10 @@ const handleComputerChange = e => {
 }
 computersElement.addEventListener("change",handleComputerChange)
 
-let joe = new bank(0,0,0);
+/**
+ * Sets initial values of bank
+ */
+let bank = new Bank(0,0,0);
 updateFields();
 
 /**
@@ -53,7 +81,7 @@ updateFields();
  * @param {number} balance 
  * @param {number} loan 
  */
-function bank(balance,loan,pay){
+function Bank(balance,loan,pay){
     this.balance=balance;
     this.loan=loan;
     this.pay=pay;
@@ -63,21 +91,21 @@ function bank(balance,loan,pay){
  * @returns number
  */
 function getBalance(){
-    return joe.balance;
+    return bank.balance;
 }
 /**
  * Adds more money to balance
  * @param {number} b 
  */
 function addBalance(b){
-    joe.balance+=b;
+    bank.balance+=b;
 }
 /**
  * Boolean for checking if there is an existing loan
  * @returns true||false
  */
 function hasLoan(){
-    if (joe.loan==0){
+    if (bank.loan==0){
         return false;
     }return true;
 }
@@ -86,13 +114,13 @@ function hasLoan(){
  * @returns number
  */
 function getPay(){
-    return joe.pay;
+    return bank.pay;
 }
 /**
  * Increase pay balance with 100kr
  */
 function work(){
-    joe.pay+=100;
+    bank.pay+=100;
     updateFields();
 }
 /**
@@ -100,14 +128,14 @@ function work(){
  * @returns number
  */
 function getLoan(){
-    return joe.loan;
+    return bank.loan;
 }
 /**
- * Checks if user has sufficient funds
+ * Checks if user has sufficient funds and buys computer if that is the case
  */
 function buy(){
     if (price<=getBalance()){
-        joe.balance-=price;
+        bank.balance-=price;
         alert("Thank you for your order, your "+selectedComputer.title+" is now beeing prepered for shipment.\nYour account will be charged shortly.");
         updateFields();
         document.getElementById('buyErrors').style.visibility='hidden';
@@ -117,66 +145,81 @@ function buy(){
     }
 
 }
+/**
+ * Sends all pay to loan, any left overs will be sent to balance
+ */
 function repay(){
     if (getPay()==getLoan()){
-        joe.loan = 0;
-        joe.pay = 0;
+        bank.loan = 0;
+        bank.pay = 0;
         updateFields();
     }
     if (getLoan()>getPay()){
-        joe.loan = joe.loan-joe.pay;
-        joe.pay = 0;
+        bank.loan = bank.loan-bank.pay;
+        bank.pay = 0;
         updateFields();
     } if (getLoan()<getPay()){
-        let rest = joe.pay-joe.loan;
-        joe.loan = 0;
-        joe.pay=0;
-        joe.balance+=rest;
+        let rest = bank.pay-bank.loan;
+        bank.loan = 0;
+        bank.pay=0;
+        bank.balance+=rest;
         updateFields();
     }
 }
+/**
+ * Transfer 90% to balance and 10% to loan, if any
+ */
 function pay() {
+    if (getPay()==0){
+        alert("You do not have any untransfered pay, you need to work in order to gain pay");
+    }
     if (getPay()*0.1==getLoan()){
-        joe.balance+=(getPay()*0.9);
-        joe.pay=0;
-        joe.loan=0;
+        bank.balance+=(getPay()*0.9);
+        bank.pay=0;
+        bank.loan=0;
         updateFields();
     }
     if ((getPay()*0.1)>getLoan()){
         repay();
-        joe.balance+=joe.pay;
-        joe.pay=0;
+        bank.balance+=bank.pay;
+        bank.pay=0;
         updateFields();
     }
     if (getPay()>0){
         let payslip=0.9*getPay();
-        joe.balance+=payslip;
-        joe.pay-=payslip;
+        bank.balance+=payslip;
+        bank.pay-=payslip;
         repay();
         updateFields(); 
     }
 }
 
+/**
+ * Lets user type inn wanted loan and checks if user is eligeble 
+ */
 function loan(){
-    
     let l = prompt("Your maximum loan amount is set to "+getBalance()*2+" kr\nRemember that you can only have one loan at any given time\nEnter amount");                    
     let amount = parseInt(l);
+    if(isNaN(amount)){amount=0;}
     if (hasLoan()){
         alert("You can only have one loan at any given time, please pay back your current loan...");
     } else if (amount>(getBalance()*2)){
         alert("Your maximum loan amount is "+getBalance()*2+" kr..");
-    }else{
-        joe.loan=amount;
-        joe.balance+=joe.loan;
+    }else {
+
+        bank.loan=amount;
+        bank.balance+=bank.loan;
         document.getElementById('loanDiv').style.visibility='visible';
         document.getElementById('loanButton').style.visibility='visible';
         updateFields();
     }
 }
 
+/**
+ * Updated fields that change throughout the program
+ */
 function updateFields(){
-    if(isNaN(joe.loan)){joe.loan=0;}
-    if(isNaN(joe.balance)){joe.balance=0;}
+    if(isNaN(bank.loan)){bank.loan=0;}
     if(!hasLoan()){document.getElementById('loanDiv').style.visibility='hidden';}
     if(!hasLoan()){document.getElementById('loanButton').style.visibility='hidden';}
     document.getElementById('balance_id').textContent = getBalance()+" kr";
